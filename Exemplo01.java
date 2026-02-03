cc5849b31df939a607ef0ccbba97dd7698ab95e3
Bianca Domingas <149802742+bianczz@users.noreply.github.com>
2026-02-03 20:47:24 -0300

    Add files via upload

--- Exemplo01.java
+++ Exemplo01.java
@@ -1,10 +1,24 @@
-package Encapsulamento;
+package Introducao;
 
 public class Exemplo01 {
 
 	public static void main(String[] args) {
-		Caneta c = new Caneta("Azul",1);
-		c.
+		Caneta bic = new Caneta("Preta",0.5);
+		Caneta montblanc = new Caneta("Azul", 2.0);
+		System.out.println("Caneta bic");
+		double x=bic.getPonta();
+		x+=5;
+		System.out.println("A ponta da caneta bic é: "+x);
+		
+		System.out.println("\nCaneta montblac");
+		montblanc.destampar();
+		montblanc.escrever();
+		montblanc.setCarga(200);
+		montblanc.estado();
+
+		
+		
+		
 	}
 
 }
